455aa002e482d9094f0755ea2879dc829f10e956
alekseydanilov <qnoize@gmail.com>
2019-12-03 17:10:43 +0300

    1st

--- src/main/java/ru/jmentor/front/app/SpringBootExampleApplication.java
+++ src/main/java/ru/jmentor/front/app/SpringBootExampleApplication.java
@@ -3,12 +3,25 @@ package ru.jmentor.front.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @ComponentScan(basePackages = "ru.jmentor.front")
 public class SpringBootExampleApplication {
 
 	public static void main(String[] args) { SpringApplication.run(SpringBootExampleApplication.class, args); }
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8075");
+			}
+		};
+	}
 
 }
